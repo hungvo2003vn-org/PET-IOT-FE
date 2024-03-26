ac955ab185800c538db677a27e288d461ca4029e
Khoa Tran <khoa.tran1805@hcmut.edu.vn>
2024-03-26 11:46:16 +0700

    added navigations

--- PetIOT/App.js
+++ PetIOT/App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import {NavigationContainer} from '@react-navigation/native';
+import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
 
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <Text>Hello world from PetIOT</Text>
-          <StatusBar style="auto" />
-        </View>
-      </NavigationContainer>
+      <DrawerNavigationRoutes/>
     </PaperProvider>
   );
 }
---
+++ PetIOT/Screen/DrawerNavigationRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import HomeScreen from './MainScreens/HomeScreen';
+import SettingsScreen from './MainScreens/SettingsScreen';
+import FeedingStationScreen from './MainScreens/FeedingStationScreen';
+import PetsScreen from './MainScreens/PetScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function DrawerNavigationRoutes(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='HomeScreen'>
+                <Stack.Screen name ="HomeScreen" component ={HomeScreen} />
+                <Stack.Screen name = "PetsScreen" component={PetsScreen}/>
+                <Stack.Screen name = "FeedingStation" component={FeedingStationScreen}/>
+                <Stack.Screen name = "SettingsScreen" component={SettingsScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
